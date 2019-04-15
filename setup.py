4dbba8f776f22ab011066a8ffbe2adbbb9830c7e
Mariano Goldman <m.goldman@aristas.com.ar>
2019-04-15 12:52:26 -0300

    Bitbucket Pipelines deploya a PyPI (testing)

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup, find_packages
 
-setup(name='fermat',
-      version='0.0.2',
-      description='library to compute fermat distance',
-      author='Facundo Sapienza',
-      author_email='f.sapienza@aristas.com.ar',
-      packages=find_packages(),
-    )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fermat',
+    version='0.0.3',
+    description='library to compute fermat distance',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://bitbucket.org/aristas/fermat',
+    license='MIT',
+    author='Facundo Sapienza',
+    author_email='f.sapienza@aristas.com.ar',
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
