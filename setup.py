a7d4cb3d3111646319e7d30d7bf13f8817c2a346
Mariano Goldman <m.goldman@aristas.com.ar>
2019-10-09 11:27:43 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements-for-release.txt', 'r') as f:
 
 setup(
     name='fermat',
-    version='0.1.0',
+    version='0.2.0',
     python_requires='>=3.5',
     description='library to compute fermat distance',
     long_description=long_description,
