46989badb3cdb14ffd13c0083346a87ffb2ac85a
Alfredo Umfurer <a.umfurer@aristas.com.ar>
2019-10-10 18:38:59 +0000

    Merged in development (pull request #9)
    Development
    
    * changing version to 0.2.2
    
    Approved-by: Mariano Goldman <m.goldman@aristas.com.ar>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements-for-release.txt', 'r') as f:
 
 setup(
     name='fermat',
-    version='0.2.1',
+    version='0.2.2',
     python_requires='>=3.5',
     description='library to compute fermat distance',
     long_description=long_description,
